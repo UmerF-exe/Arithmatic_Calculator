21195514108f982495338880662b24433445a6f0
Umer Farooque <mohranvi4484@gmail.com>
2025-06-13 16:41:54 +0500

    updated

---
+++ app.js
@@ -0,0 +1,38 @@
+var result = document.getElementById("display");
+
+function numberDot(){
+    result.value += document.getElementById("numberdot").value;
+}
+function number0(){
+    result.value += document.getElementById("number0").value;
+}
+function number00(){
+    result.value += document.getElementById("number00").value;
+}
+function number1(){
+    result.value += document.getElementById("number1").value;
+}
+function number2(){
+    result.value += document.getElementById("number2").value;
+}
+function number3(){
+    result.value += document.getElementById("number3").value;
+}
+function number4(){
+    result.value += document.getElementById("number4").value;
+}
+function number5(){
+   result.value += document.getElementById("number5").value;
+}
+function number6(){
+    result.value += document.getElementById("number6").value;
+}
+function number7(){
+    result.value += document.getElementById("number7").value;
+}
+function number8(){
+    result.value += document.getElementById("number8").value;
+}
+function number9(){
+    result.value += document.getElementById("number9").value;
+}
