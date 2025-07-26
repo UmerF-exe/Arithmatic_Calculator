8ea0539ca86f44e85154ad17bb76bcab796d5859
Umer Farooque <mohranvi4484@gmail.com>
2025-07-26 10:11:01 +0500

    updated

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
 var result = document.getElementById("display");
 var expression = document.getElementById("result");
 
-function numberDot(){
-    result.value += document.getElementById("numberdot").value;
+function getNum(num){
+    if(num != "."){
+        result.value += Number(num);
+    }
+    else{
+        result.value += num;
+    }
 }
-function number0(){
-    result.value += document.getElementById("number0").value;
-}
-function number00(){
-    result.value += document.getElementById("number00").value;
-}
-function number1(){
-    result.value += document.getElementById("number1").value;
-}
-function number2(){
-    result.value += document.getElementById("number2").value;
-}
-function number3(){
-    result.value += document.getElementById("number3").value;
-}
-function number4(){
-    result.value += document.getElementById("number4").value;
-}
-function number5(){
-   result.value += document.getElementById("number5").value;
-}
-function number6(){
-    result.value += document.getElementById("number6").value;
-}
-function number7(){
-    result.value += document.getElementById("number7").value;
-}
-function number8(){
-    result.value += document.getElementById("number8").value;
-}
-function number9(){
-    result.value += document.getElementById("number9").value;
-}
-function addition(){
-    result.value += document.getElementById("operator+").value;
-}
-function subtraction(){
-    result.value += document.getElementById("operator-").value;
-}
-function multiplication(){
-    result.value += document.getElementById("operator*").value;
-}
-function division(){
-    result.value += document.getElementById("operator/").value;
-}
-function calculate(){
-    expression.value = result.value;
-    result.value = eval(result.value);
-    
-}
-function clearAll(){
-    result.value = " ";
-    expression.value = " ";
-}
-function deleteBtn(){
-    result.value = result.value.toString().slice(0,-1);
-    if(result.value == 0){
-        expression.value = " ";
+
+function operation(op){
+    switch(op){
+        case "+":
+            result.value += "+";
+            break;
+        case "-":
+            result.value += "-";
+            break;
+        case "*":
+            result.value += "*";
+            break;
+        case "/":
+            result.value += "/";
+            break;
+        case "%":
+            result.value += "%";
+            break;
+        case "AC":
+            result.value = " ";
+            expression.value = " ";
+            break;
+        case "DE":
+            result.value = result.value.toString().slice(0,-1);
+            if(result.value == 0){
+                expression.value = " ";
+            }
+            break;
+        default:
+            expression.value = result.value;
+            result.value = eval(result.value);
     }
 }
