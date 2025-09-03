bb0eb006d530b19e8685be7fa955a7e9b0050f7d
Umer Farooque <mohranvi4484@gmail.com>
2025-09-03 16:54:16 +0500

    optimized the history and fixes the other bugs

--- app.js
+++ app.js
@@ -13,56 +13,105 @@ window.onload = function() {
 };
 
 function getNum(num){
-    if(num != "."){
-        display.value += Number(num);
+    if(num == "."){
+        let parts = display.value.split(/[\+\-\*\/%]/);
+        let currentNumber = parts[parts.length - 1];
+        if (currentNumber.includes(".")) {
+            return;
+        }
+        if (currentNumber.trim() === "") {
+            display.value += "0.";
+            updateLiveResult();
+        } else {
+            display.value += ".";
+            updateLiveResult();
+        }
     }
     else{
-        display.value += num;
+        display.value += Number(num);
+        updateLiveResult();
     }
 }
 
 function operation(op){
-
-    if(!display.value){
-        alert("Invalid Input! Enter any number before performing any operation");
-    }
-    else{
-        switch(op){
-            case "+":
-                display.value += " + ";
-                break;
-            case "-":
-                display.value += " - ";
-                break;
-            case "*":
-                display.value += " * ";
-                break;
-            case "/":
-                display.value += " / ";
-                break;
-            case "%":
-                display.value += " % ";
-                break;
-            case "AC":
-                display.value = " ";
+    let lastChar = display.value.trim().slice(-1)
+    switch(op){
+        case "+":
+        case "*":
+        case "/":
+        case "%":
+            if (display.value.trim() === "") {
+                alert("You must enter a number first!");
+                return;
+            }
+            if(["+", "-", "*", "/", "%"].includes(lastChar)) {
+                display.value = display.value.trim().slice(0, -1) + op + " ";
+                updateLiveResult();
+            }
+            else {
+                display.value += " " + op + " ";
+                updateLiveResult();
+            }
+            break;
+        case "-":
+            if (display.value.trim() === "") {
+                display.value = "-";
+                updateLiveResult();
+            }
+            else if(["+", "*", "/", "%"].includes(lastChar)) {
+                display.value = display.value.trim().slice(0, -1) + op + " ";
+                updateLiveResult();
+            } else {
+                display.value += " " + op + " ";
+                updateLiveResult();
+            }
+            break;
+        case "AC":
+            display.value = " ";
+            result.value = " ";
+            updateLiveResult();
+            break;
+        case "DE":
+            display.value = display.value.toString().slice(0,-1);
+            updateLiveResult();
+            if(display.value.trim() == ""){
                 result.value = " ";
-                break;
-            case "DE":
-                display.value = display.value.toString().slice(0,-1);
-                if(display.value == 0){
-                    result.value = " ";
-                }
-                break;
-            default:
-                result.value = display.value;
-                display.value = eval(display.value);
-                let entry = "<p>" + result.value + " = " + display.value + "</p><hr/>";
+                updateLiveResult();
+            }
+            break;
+        default:
+            try{
+                let fullExpr = display.value;
+                let answer = eval(fullExpr);
+                result.value = fullExpr;
+                display.value = answer; 
+                updateLiveResult();
+                let entry = "<p>" + fullExpr + " = " + answer + "</p><hr/>";
                 historyContent.innerHTML += entry;
                 localStorage.setItem("History", historyContent.innerHTML);
-        }
+            }
+            catch(e){
+                alert("Invalid Expression");
+            }
     }
 }
 
+function updateLiveResult() {
+    try {
+        let expr = display.value.trim();
+        if (expr && !["+", "-", "*", "/", "%", "."].includes(expr.slice(-1))) {
+            result.value = eval(expr);
+        } else {
+            result.value = "";
+        }
+    } catch {
+        result.value = "";
+    }
+}
+
+function saveHistory() {
+    localStorage.setItem("History", historyContent.innerHTML);
+}
 function showHistory(){
     closeIcon.style.display = "block";
     historyIcon.style.display = "none";
@@ -76,4 +125,6 @@ function hideHistory(){
 function deleteHistory(){
     historyContent.innerHTML = "";
     localStorage.removeItem("History");
+    result.value = "";
+    display.value = "";
 }
